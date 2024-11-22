efeda0b2b2e67be6a696692689c87c524f909a04
Prasad Rayabhagi <lraya127@marriott.com>
2024-11-21 21:24:25 -0600

    changed the data

--- src/routes/data.ts
+++ src/routes/data.ts
@@ -1,41 +1,43 @@
-export const employees = [{
-    id: 3,
-    firstName: 'Tony',
-    lastName: 'Stark',
-    organization: 'Marvel'
+const employees = [
+  {
+    empId: 3,
+    firstName: "Tony",
+    lastName: "Stark",
+    organization: "Marvel",
   },
   {
-    id: 1,
-    firstName: 'Bruce',
-    lastName: 'Banner',
-    organization: 'Marvel'
+    empId: 1,
+    firstName: "Bruce",
+    lastName: "Banner",
+    organization: "Marvel",
   },
   {
-    id: 2,
-    firstName: 'Bruce',
-    lastName: 'Wayne',
-    organization: 'DC'
+    empId: 2,
+    firstName: "Bruce",
+    lastName: "Wayne",
+    organization: "DC",
   },
   {
-    id: 5,
-    firstName: 'Clark',
-    lastName: 'Kent',
-    organization: 'DC'
+    empId: 5,
+    firstName: "Clark",
+    lastName: "Kent",
+    organization: "DC",
   },
   {
-    id: 4,
-    firstName: 'John',
-    lastName: 'Stark',
-    organization: 'Marvel'
-}];
+    empId: 4,
+    firstName: "John",
+    lastName: "Stark",
+    organization: "Marvel",
+  },
+];
 
-export const cities = [
+const hometowns = [
   {
-  id: 1,
-  city: 'Dayton'
+    empId: 1,
+    city: "Dayton",
   },
   {
-  id: 3,
-  city: 'Malibu'
-  }
+    empId: 3,
+    city: "Malibu",
+  },
 ];
