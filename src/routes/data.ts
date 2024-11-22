c560d59c17ae8d3034b5828422b7b2202f29da53
Prasad Rayabhagi <lraya127@marriott.com>
2024-11-21 21:13:22 -0600

    did some corrections

---
+++ src/routes/data.ts
@@ -0,0 +1,41 @@
+export const employees = [{
+    id: 3,
+    firstName: 'Tony',
+    lastName: 'Stark',
+    organization: 'Marvel'
+  },
+  {
+    id: 1,
+    firstName: 'Bruce',
+    lastName: 'Banner',
+    organization: 'Marvel'
+  },
+  {
+    id: 2,
+    firstName: 'Bruce',
+    lastName: 'Wayne',
+    organization: 'DC'
+  },
+  {
+    id: 5,
+    firstName: 'Clark',
+    lastName: 'Kent',
+    organization: 'DC'
+  },
+  {
+    id: 4,
+    firstName: 'John',
+    lastName: 'Stark',
+    organization: 'Marvel'
+}];
+
+export const cities = [
+  {
+  id: 1,
+  city: 'Dayton'
+  },
+  {
+  id: 3,
+  city: 'Malibu'
+  }
+];
